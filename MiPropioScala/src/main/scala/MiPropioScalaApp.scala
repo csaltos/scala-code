5c8a11a0a07f8e7d220aa864b9687b488ea64c03
Carlos Saltos <csaltos@gmail.com>
2022-10-27 03:27:30 -0500

    Mi propio Scala

--- MiPropioScala/src/main/scala/MiPropioScalaApp.scala
+++ MiPropioScala/src/main/scala/MiPropioScalaApp.scala
@@ -1,15 +1,30 @@
 sealed trait Opcion[+A] {
+
   def estaVacio: Boolean
+
+  def estaLleno: Boolean = !estaVacio
+
+  def leerOValorPorDefecto[B >: A](valorPorDefecto: => B): B = {
+    if (estaLleno) {
+      leer
+    } else {
+      valorPorDefecto
+    }
+  }
+
   def leer: A
+
 }
 
 case class Algo[+A](valor: A) extends Opcion[A] {
   def estaVacio = false
+  //def leerOValorPorDefecto[B >: A](valorPorDefecto: => B): B = valor
   def leer: A = valor
 }
 
 case object Ninguno extends Opcion[Nothing] {
   def estaVacio = true
+  //def leerOValorPorDefecto[B](valorPorDefecto: => B): B = valorPorDefecto
   def leer: Nothing = throw new IllegalStateException("Error llamando a una opción vacía")
 }
 
@@ -18,6 +33,16 @@ object MiPropioScalaApp extends App {
   println("Bienvenido a mi propio Scala")
 
   def maximo: Opcion[Int] = Ninguno
+
+  val maximoValor = maximo.leerOValorPorDefecto(1000)
+
+  println("Valor de máximo efectivo: " + maximoValor)
+
+  if (maximo.estaLleno) {
+    println("El máximo es " + maximo.leer)
+  } else {
+    println("No hay máximo")
+  }
 
   if (maximo.estaVacio) {
     println("No hay máximo")
