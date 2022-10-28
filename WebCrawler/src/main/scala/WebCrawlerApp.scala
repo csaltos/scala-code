40a918e8e57782491531ec113b2dee8c2221e29b
Carlos Saltos <csaltos@gmail.com>
2022-10-28 06:56:38 -0500

    WebCrawler

--- WebCrawler/src/main/scala/WebCrawlerApp.scala
+++ WebCrawler/src/main/scala/WebCrawlerApp.scala
@@ -1,15 +1,35 @@
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 import scala.io.Source
 
 object WebCrawlerApp extends App {
-
-  def crawl(url: String): String = {
-    val cabecera = Source.fromURL(url).getLines().toList.take(3)
-    cabecera.mkString("\n")
+  def crawl(url: String): Future[String] = {
+    Future {
+      val cabecera = Source.fromURL(url).getLines().toList.take(3)
+      cabecera.mkString("\n")
+    }
   }
 
-  val contenido = crawl("https://www.yahoo.com/")
+  def load(nombre: String, url: String): Future[Boolean] = {
+    crawl(url) map { contenido =>
+      println(s"${nombre}: " + contenido)
+      true
+    } recover {
+      case error =>
+        println(s"No se pudo cargar ${nombre}: " + error.getMessage)
+        false
+    }
+  }
 
-  println(contenido)
+  val resultados = Seq(
+    load("Yahoo", "https://www.yahoo.com/"),
+    load("Google", "https://www.google.com/"),
+    load("IBM", "https://www.ibm.com/")
+  )
+
+  Future.sequence(resultados)
+
+  Await.ready(Future.never, Duration.Inf)
 
 }
